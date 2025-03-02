ad74a02579420041242037b32963c0b79fe1d0ad
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-03-02 23:02:55 +0000

    code clean up

--- apache-spark-java-v1/src/main/java/com/dekapx/spark/quickstart/SparkWordCount.java
+++ apache-spark-java-v1/src/main/java/com/dekapx/spark/quickstart/SparkWordCount.java
@@ -4,6 +4,8 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import static org.apache.spark.sql.functions.*;
+
 public class SparkWordCount {
     public static void main(String[] args) {
         SparkSession sparkSession = SparkSession
@@ -13,7 +15,26 @@ public class SparkWordCount {
                 .getOrCreate();
 
         Dataset<Row> dataFrame = readData(sparkSession);
+        printData(dataFrame);
+        printSchema(dataFrame);
+        printModifiedData(dataFrame);
+    }
+
+    private static void printData(Dataset<Row> dataFrame) {
         dataFrame.show();
+    }
+
+    private static void printSchema(Dataset<Row> dataFrame) {
+        dataFrame.printSchema();
+    }
+
+    private static void printModifiedData(Dataset<Row> dataFrame) {
+        Dataset<Row> modifiedDataFrame = dataFrame
+                .withColumn("full_name",
+                        concat(col("first_name"),
+                                lit(", "),
+                                col("last_name")));
+        modifiedDataFrame.show();
     }
 
     private static Dataset<Row> readData(SparkSession sparkSession) {
