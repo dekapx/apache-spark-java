7025679e1e596073274d8b96308e63785a1b23ff
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-04-09 23:02:59 +0100

    code clean up

---
+++ apache-spark-java-v1/src/main/java/com/dekapx/spark/model/Car.java
@@ -0,0 +1,12 @@
+package com.dekapx.spark.model;
+
+public class Car {
+    private String make;
+    private String model;
+    private String year;
+    private String color;
+    private String transmission;
+    private String fuelType;
+    private int mileage;
+    private double price;
+}
---
+++ apache-spark-java-v1/src/main/java/com/dekapx/spark/quickstart/SparkJsonReadPojoMapper.java
@@ -0,0 +1,46 @@
+package com.dekapx.spark.quickstart;
+
+import com.dekapx.spark.model.Car;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * vm-args: --add-exports java.base/sun.nio.ch=ALL-UNNAMED
+ */
+public class SparkJsonReadPojoMapper {
+    public static final String HEADER = "header";
+    public static final String MULTILINE = "multiline";
+    public static final String JSON_FILE_PATH = "src/main/resources/data/car-inventory.json";
+
+    public static void main(String[] args) {
+        SparkSession sparkSession = createSparkSession();
+        Dataset<Row> dataFrame = buildDataFrame(sparkSession);
+        mapToPojo(dataFrame);
+    }
+
+    private static void mapToPojo(Dataset<Row> dataFrame) {
+        Dataset<Car> carDataset = dataFrame.as(Encoders.bean(Car.class));
+        carDataset.printSchema();
+        carDataset.show();
+    }
+
+    private static Dataset<Row> buildDataFrame(SparkSession sparkSession) {
+        Dataset<Row> dataFrame = sparkSession
+                .read()
+                .option(HEADER, true)
+                .option(MULTILINE, true)
+                .json(JSON_FILE_PATH);
+        return dataFrame;
+    }
+
+    private static SparkSession createSparkSession() {
+        SparkSession sparkSession = SparkSession
+                .builder()
+                .appName("SparkJsonReadPojoMapper")
+                .master("local[*]")
+                .getOrCreate();
+        return sparkSession;
+    }
+}
