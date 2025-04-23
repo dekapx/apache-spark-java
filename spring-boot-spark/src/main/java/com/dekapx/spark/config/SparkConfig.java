2aa967c8189396d356b2817a9efad83da348d939
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-04-23 23:32:04 +0100

    code clean up

--- spring-boot-spark/src/main/java/com/dekapx/spark/config/SparkConfig.java
+++ spring-boot-spark/src/main/java/com/dekapx/spark/config/SparkConfig.java
@@ -1,7 +1,16 @@
 package com.dekapx.spark.config;
 
+import org.apache.spark.sql.SparkSession;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SparkConfig {
+     @Bean
+     public SparkSession sparkSession() {
+         return SparkSession.builder()
+                 .appName("Spark App")
+                 .master("local[*]")
+                 .getOrCreate();
+     }
 }
--- spring-boot-spark/src/main/java/com/dekapx/spark/service/CsvReaderService.java
+++ spring-boot-spark/src/main/java/com/dekapx/spark/service/CsvReaderService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -13,22 +14,15 @@ public class CsvReaderService {
     public static final String CSV_FILE_PATH = "src/main/resources/data/movies-data.txt";
     public static final String FILE_HEADER = "header";
 
+    @Autowired
     private SparkSession sparkSession;
 
     public void readCsvFile() {
         log.info("Reading CSV file...");
-        createSparkSession();
         Dataset<Row> dataFrame = buildDataFrame(sparkSession);
         dataFrame.printSchema();
         dataFrame.show();
         log.info("CSV file read successfully.");
-    }
-
-    private void createSparkSession() {
-        this.sparkSession = SparkSession.builder()
-                .appName("Spring Boot Spark App")
-                .master("local[*]")
-                .getOrCreate();
     }
 
     private static Dataset<Row> buildDataFrame(SparkSession sparkSession) {
