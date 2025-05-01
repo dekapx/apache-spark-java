c1939c3bc1ccf73d246094a919f9e86055b85b1a
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-05-01 12:28:11 +0100

    code clean up

--- spring-boot-spark/src/main/java/com/dekapx/spark/config/SparkConfig.java
+++ spring-boot-spark/src/main/java/com/dekapx/spark/config/SparkConfig.java
@@ -1,16 +1,23 @@
 package com.dekapx.spark.config;
 
 import org.apache.spark.sql.SparkSession;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SparkConfig {
-     @Bean
-     public SparkSession sparkSession() {
-         return SparkSession.builder()
-                 .appName("Spark App")
-                 .master("local[*]")
-                 .getOrCreate();
-     }
+    @Value("${spark.app.name}")
+    private String sparkAppName;
+
+    @Value("${spark.app.master}")
+    private String sparkAppMaster;
+
+    @Bean
+    public SparkSession sparkSession() {
+        return SparkSession.builder()
+                .appName(sparkAppName)
+                .master(sparkAppMaster)
+                .getOrCreate();
+    }
 }
