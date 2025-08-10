f2f8fd7750dbae5820e0fb23a1b374679693b7ff
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-08-10 16:07:42 +0100

    apache spark java samples

---
+++ apache-spark-quickstart/src/main/java/com/dekapx/java/chapter02/README.md
@@ -0,0 +1,5 @@
+# Apache Spark 4 & Java 21 
+## Read a CSV file and print contents with schema
+## Add new column to the DataFrame
+
+
---
+++ apache-spark-quickstart/src/main/java/com/dekapx/java/chapter02/ReadCsvAddNewColumn.java
@@ -0,0 +1,53 @@
+package com.dekapx.java.chapter02;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.concat;
+import static org.apache.spark.sql.functions.lit;
+
+public class ReadCsvAddNewColumn {
+    public static void main(String[] args) {
+        SparkSession spark = createSparkSession();
+
+        // Read a CSV file and print its schema and data
+        Dataset<Row> dataset = readCsvFile(spark);
+        dataset.printSchema();
+        dataset.show(5, false); // Show 5 rows without truncating
+
+        // Add a new column 'full_name' by concatenating 'first_name' and 'last_name'
+        dataset = dataset
+                .withColumn("full_name",
+                        concat(
+                                col("first_name"),
+                                lit(", "),
+                                col("last_name")));
+        dataset.printSchema();
+        dataset.show();
+
+        // Drop the 'first_name' and 'last_name' columns
+        dataset = dataset.drop("first_name", "last_name");
+        dataset.printSchema();
+        dataset.show();
+
+        spark.stop();
+    }
+
+    private static Dataset<Row> readCsvFile(SparkSession spark) {
+        String filePath = "src/main/resources/chapter02/data.csv";
+        return spark.read()
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .csv(filePath);
+    }
+
+    private static SparkSession createSparkSession() {
+        return SparkSession
+                .builder()
+                .appName("ApacheSparkHelloWorld")
+                .master("local[*]")
+                .getOrCreate();
+    }
+}
